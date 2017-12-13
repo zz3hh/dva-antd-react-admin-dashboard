1ac38daa852fa9aef30afb5d6af2579b2c542a7a
zz3hh <879974693@qq.com>
2017-12-13 21:39:13 +0800

    update

---
+++ src/common/nav.js
@@ -0,0 +1,67 @@
+import dynamic from 'dva/dynamic';
+
+// wrapper of dynamic
+const dynamicWrapper = (app, models, component) => dynamic({
+  app,
+  models: () => models.map(m => import(`../models/${m}.js`)),
+  component,
+});
+
+// nav data
+export const getNavData = app => [
+  {
+    component: dynamicWrapper(app, ['user', 'login'], () => import('../layouts/BasicLayout')),
+    layout: 'BasicLayout',
+    name: '首页', // for breadcrumb
+    path: '/',
+    children: [
+      {
+        name: 'Dashboard',
+        icon: 'dashboard',
+        path: 'dashboard',
+        children: [
+          {
+            name: '分析页',
+            path: 'analysis',
+            component: dynamicWrapper(app, ['chart'], () => import('../routes/Dashboard/Analysis')),
+          },
+          {
+            name: '监控页',
+            path: 'monitor',
+            component: dynamicWrapper(app, ['monitor'], () => import('../routes/Dashboard/Monitor')),
+          },
+          {
+            name: '工作台',
+            path: 'workplace',
+            component: dynamicWrapper(app, ['project', 'activities', 'chart'], () => import('../routes/Dashboard/Workplace')),
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: dynamicWrapper(app, [], () => import('../layouts/UserLayout')),
+    path: '/user',
+    layout: 'UserLayout',
+    children: [
+      {
+        name: '帐户',
+        icon: 'user',
+        path: 'user',
+        children: [
+          
+        ],
+      },
+    ],
+  },
+  {
+    component: dynamicWrapper(app, [], () => import('../layouts/BlankLayout')),
+    layout: 'BlankLayout',
+    children: {
+      name: '使用文档',
+      path: 'http://pro.ant.design/docs/getting-started',
+      target: '_blank',
+      icon: 'book',
+    },
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
+import 'babel-polyfill';
 import dva from 'dva';
-import './index.css';
+import 'moment/locale/zh-cn';
+import './g2';
+import './rollbar';
+// import browserHistory from 'history/createBrowserHistory';
+import './index.less';
+import router from './router';
 
 // 1. Initialize
-const app = dva();
-
-app.model(require("./models/product"));
+const app = dva({
+  // history: browserHistory(),
+});
 
 // 2. Plugins
 // app.use({});
 
-// 3. Model
-// app.model(require('./models/example'));
+// 3. Register global model
+app.model(require('./models/global'));
 
 // 4. Router
-app.router(require('./router'));
+app.router(router);
 
 // 5. Start
 app.start('#root');
